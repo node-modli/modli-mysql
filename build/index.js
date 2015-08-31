b636e63170be764962c3133465fffdaf3f8f0f3c
fluidbyte <kent@fluidbyte.net>
2015-08-31 15:47:30 -0400

    Pre-1.0.0 build

--- build/index.js
+++ build/index.js
@@ -4,97 +4,169 @@ Object.defineProperty(exports, '__esModule', {
   value: true
 });
 var Promise = require('bluebird');
-var Datastore = require('nedb');
+var mysqlModule = require('mysql');
 
 /**
- * @namespace nedb
+ * @namespace mysql
  */
-var nedb = {};
+var mysql = {};
 
-exports.nedb = nedb;
+exports.mysql = mysql;
 /**
- * Accepts config parameters
- * @memberof nedb
- * @param {Object} cfg Configuration
+ * @propery {Object} conn The MySQL connection
  */
-nedb.config = function (cfg) {
-  nedb.db = Promise.promisifyAll(new Datastore(cfg));
-  return true;
+mysql.conn = {};
+
+/**
+ * Pass-through to direct query (promisified)
+ * @memberof mysql
+ * @param {String} query The query to run
+ */
+mysql.query = function (query) {
+  return new Promise(function (resolve, reject) {
+    mysql.conn.query(query, function (err, result) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
 };
 
 /**
- * Creates a new entry in the database
- * @memberof nedb
- * @param {Object} body Contents to create entry
- * @param {String|Number} [version] The version of the model to use
+ * Configs the MySQL Connection
+ * @memberof mysql
+ * @param {Object} cfg The config object
+ *   @property {String} cfg.host
+ *   @property {String} cfg.user
+ *   @property {String} cfg.password
+ *   @property {String} cfg.database
+ */
+mysql.config = function (cfg) {
+  mysql.conn = mysqlModule.createConnection(cfg);
+  mysql.conn.connect(function (err) {
+    if (err) {
+      throw new Error('MySQL Connection Error', err);
+    }
+  });
+};
+
+/**
+ * Creates a table
+ * @memberof mysql
+ * @param {String} name The name of the table to create
+ * @param {Object} props The properties of the table
  * @returns {Object} promise
  */
-nedb.create = function (body) {
+mysql.createTable = function (props) {
+  // Build query
+  var len = Object.keys(props).length;
+  var i = 1;
+  var query = 'CREATE TABLE IF NOT EXISTS ' + mysql.tableName + ' (';
+  for (var prop in props) {
+    var comma = i !== len ? ', ' : '';
+    query += prop + ' ' + props[prop].join(' ') + comma;
+    i++;
+  }
+  query += ');';
+  // Run query
+  return mysql.query(query);
+};
+
+/**
+ * Creates a new record
+ * @memberof mysql
+ * @param {Object} body The record to insert
+ * @param {Sting|Number} [version] The version of the model
+ * @returns {Object} promise
+ */
+mysql.create = function (body) {
   var version = arguments.length <= 1 || arguments[1] === undefined ? false : arguments[1];
 
-  // Test validation
-  var validationErrors = nedb.validate(body, version);
-  // Return promise
   return new Promise(function (resolve, reject) {
-    /* istanbul ignore if */
+    // Validate
+    var validationErrors = mysql.validate(body, version);
     if (validationErrors) {
       reject(validationErrors);
     } else {
-      resolve(nedb.db.insertAsync(body));
+      // Build query
+      var cols = [];
+      var vals = [];
+      for (var prop in body) {
+        cols.push(prop);
+        vals.push('"' + body[prop] + '"');
+      }
+      var query = 'INSERT INTO ' + mysql.tableName + ' (' + cols.join(',') + ') VALUES (' + vals.join(',') + ');';
+      // Run query
+      resolve(mysql.query(query));
     }
   });
 };
 
 /**
- * Reads from the database
- * @memberof nedb
- * @param {Object} query Specific id or query to construct read
+ * Queries for a record
+ * @memberof mysql
+ * @param {Object} query The query to execute
  * @returns {Object} promise
  */
-nedb.read = function (query) {
-  return nedb.db.findAsync(query);
+mysql.read = function (query) {
+  var where = undefined;
+  if (query) {
+    where = ' WHERE ' + query;
+  } else {
+    where = '';
+  }
+  return mysql.query('SELECT * FROM ' + mysql.tableName + where);
 };
 
 /**
- * Updates an entry in the database
- * @memberof nedb
- * @param {String} query Query to locate entries to update
- * @param {Object} body Contents to update
- * @param {String|Number} [version] The version of the model to use
+ * Updates an existing record
+ * @memberof mysql
+ * @param {Object} query The query to identify update record(s)
+ * @params {Object} body The record contents to update
+ * @params {String|Number} version The version of the model
  * @returns {Object} promise
  */
-nedb.update = function (query, body) {
+mysql.update = function (query, body) {
   var version = arguments.length <= 2 || arguments[2] === undefined ? false : arguments[2];
 
-  // Test validation
-  var validationErrors = nedb.validate(body, version);
-  // Return promise
   return new Promise(function (resolve, reject) {
-    /* istanbul ignore if */
+    var validationErrors = mysql.validate(body, version);
     if (validationErrors) {
       reject(validationErrors);
     } else {
-      resolve(nedb.db.updateAsync(query, { $set: body }, { multi: true }));
+      var i = 1;
+      var changes = '';
+      var len = Object.keys(body).length;
+      for (var prop in body) {
+        if (({}).hasOwnProperty.call(body, prop)) {
+          var comma = i !== len ? ', ' : '';
+          changes += prop + '="' + body[prop] + '"' + comma;
+          i++;
+        }
+      }
+      resolve(mysql.query('UPDATE ' + mysql.tableName + ' SET ' + changes + ' WHERE ' + query));
     }
   });
 };
 
 /**
- * Deletes an item from the database
- * @memberof nedb
- * @param {Object} query Query to locate entries to delete
+ * Deletes a record
+ * @memberof mysql
+ * @param {Object} query
  * @returns {Object} promise
  */
-nedb['delete'] = function (query) {
-  return nedb.db.removeAsync(query, { multi: true });
+mysql['delete'] = function (query) {
+  return mysql.query('DELETE FROM ' + mysql.tableName + ' WHERE ' + query);
 };
 
 /**
- * Extends adapter by adding new method
- * @memberof nedb
+ * Extends the mysql object
+ * @memberof mysql
  * @param {String} name The name of the method
- * @param {Function} fn The method to add
+ * @param {Function} fn The function to extend on the object
  */
-nedb.extend = function (name, fn) {
-  nedb[name] = fn.bind(nedb);
+mysql.extend = function (name, fn) {
+  mysql[name] = fn.bind(mysql);
 };
