6cebd3fb360761866137321f879916af891e95d3
fluidbyte <kent@fluidbyte.net>
2015-10-27 16:27:16 -0400

    Pre-1.1.2 build

--- build/index.js
+++ build/index.js
@@ -9,7 +9,7 @@ var _createClass = (function () { function defineProperties(target, props) { for
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 var Promise = require('bluebird');
-var mysqlModule = require('mysql2');
+var mysqlModule = require('mysql');
 
 /**
  * @class mysql
